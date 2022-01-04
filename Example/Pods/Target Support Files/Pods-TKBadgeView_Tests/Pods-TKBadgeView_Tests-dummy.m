b05dd3f59ca113bb3fcdecd658a3a3a1bc0eb30b
Timm Kent <timm.kent@yahoo.com>
2022-01-04 08:22:21 +0100

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-TKBadgeView_Tests/Pods-TKBadgeView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TKBadgeView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TKBadgeView_Tests
+@end
---
+++ Example/Pods/Target Support Files/TKBadgeView/TKBadgeView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TKBadgeView : NSObject
+@end
+@implementation PodsDummy_TKBadgeView
+@end
